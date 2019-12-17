ae3e6027c740560a13014c62db9be715e394d669
Jakub Nowak <jakubnowak712@gmail.com>
2019-12-17 22:39:02 +0100

    jalg.go: zapisywanie liczb w formie Little Endian i padding

--- src/jalg/jalg.go
+++ src/jalg/jalg.go
@@ -1,6 +1,10 @@
+// jalg - kompilator plików JSON do formatu .alg wspieranego przez
+// Magiczne Bloczki®
+
 package main
 
 import (
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -18,9 +22,21 @@ type Block struct {
 }
 
 var Blocks *[]Block
+
 var filename string = "./test.json"
 var buf []byte
 var fBeginning = []byte{0x3e, 0x67, 0x01, 0x0a}
+
+// Ta funkcja będzie prawdopodobnie do usunięcia (bo LittleEndian),
+// ale zostawię, aż dojdę do treści bloczka, bo może się przydać
+func pad(b []byte, l int) ([]byte, error) {
+	if l < len(b) {
+		return b, fmt.Errorf("pad: Podana długość %d jest mniejsza niż długość wycinka %d.", l, len(b))
+	}
+	padding := make([]byte, l-len(b))
+	result := append(b, padding...)
+	return result, nil
+}
 
 func main() {
 	file, err := ioutil.ReadFile(filename)
@@ -30,7 +46,8 @@ func main() {
 	}
 	json.Unmarshal(file, &Blocks)
 
-	bTypeMap := map[string]uint32{
+	// TODO: Zmienić z powrotem na uint32
+	bTypeMap := map[string]byte{
 		"start":   1,
 		"stop":    2,
 		"data":    3,
@@ -40,17 +57,29 @@ func main() {
 		"comment": 7,
 	}
 
+	fBeginning, err := pad(fBeginning, 2)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	for _, b := range *Blocks {
-		fmt.Println(bTypeMap[b.Type])
+
+		sType, err := pad([]byte{bTypeMap[b.Type]}, 4)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(sType)
+		bBuf := make([]byte, 16)
+		binary.LittleEndian.PutUint32(bBuf[0:], 0x01ef)
+		fmt.Println(bBuf)
+
 	}
 
 	fmt.Printf("%x\n", bTypeMap["data"])
 
 	buf = append(fBeginning)
 	outf := "wynik.alg"
-
 	ioutil.WriteFile(outf, buf, 0644)
 
 }
-
-//TODO: funkcja dodająca padding do bajtów
